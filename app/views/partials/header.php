56388f24210567c964c16ff775fe1ab0fb10b1c6
Brandon <brandonfrith@hotmail.co.uk>
2022-09-15 16:56:36 +0100

    add themes folder for tidiness

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/style/style.css">
-    <link rel="stylesheet" href="/public/style/<?= $styleChoice ?>theme.css">
+    <link rel="stylesheet" href="/public/style/themes/<?= $styleChoice ?>theme.css">
     <title>Cars Directory</title>
 </head>
 
