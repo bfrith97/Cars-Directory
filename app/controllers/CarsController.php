05de8c593c238475e3019273efeac2d8a08c1ee4
Brandon <brandonfrith@hotmail.co.uk>
2022-09-16 12:04:44 +0100

    edit form validation

--- app/controllers/CarsController.php
+++ app/controllers/CarsController.php
@@ -15,9 +15,12 @@ function validateInput($data)
 
 function validateBrand($data)
 {
-    if (!$data === 'bmw' ||  !$data === 'audi' ||  !$data === 'mercedes' ||  !$data === 'jaguar' ||  !$data === 'ford' ||  !$data === 'mazda') {
-        print_r('error');
+    if ($data !== 'bmw' ||  $data !== 'audi' ||  $data !== 'mercedes' ||  $data !== 'jaguar' ||  $data !== 'ford' ||  $data !== 'mazda') {
+        return 'unknown';
     } else {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
         return $data;
     }
 }
@@ -26,7 +29,6 @@ class CarsController
 {
     public function index()
     {
-
         $bmws = App::get('database')->selectAll('bmw');
         $audis = App::get('database')->selectAll('audi');
         $mercs = App::get('database')->selectAll('mercedes');
@@ -46,7 +48,7 @@ class CarsController
     public function store()
     {
 
-        $brand = validateBrand($_POST['brand']);
+        $brand = ($_POST['brand']);
         App::get('database')->insert($brand, [
             'brand' => validateBrand($_POST['brand']),
             'model' => validateInput($_POST['model']),
@@ -62,19 +64,19 @@ class CarsController
         return view('edit', [
             'brand' => validateInput($_POST['brand']),
             'id' => validateInput($_POST['id']),
-            'model' => $_POST['model'],
-            'year' => $_POST['year'],
-            'type' => $_POST['type']
+            'model' => validateInput($_POST['model']),
+            'year' => validateInput($_POST['year']),
+            'type' => validateInput($_POST['type'])
         ]);
     }
 
     public function update()
     {
-        $brand = $_POST['brand'];
-        $id = $_POST['id'];
-        $model = $_POST['model'];
-        $year = $_POST['year'];
-        $type = $_POST['type'];
+        $brand = validateBrand($_POST['brand']);
+        $id = validateInput($_POST['id']);
+        $model = validateInput($_POST['model']);
+        $year = validateInput($_POST['year']);
+        $type = validateInput($_POST['type']);
 
         App::get('database')->update($brand, $model, $year, $type, $id);
 
